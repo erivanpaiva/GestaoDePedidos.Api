48fba7fee81b1ddb9b072972688241f5c17f4353
Erivan Paiva <jetpfilho@gmail.com>
2026-02-11 20:19:47 -0300

    feat: configure GestaoDePedidosDbContext and Sqlite connection

---
+++ GestaoDePedidos.Api/Data/GestaoDePedidosDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestaoDePedidos.Api.Models;
+
+namespace GestaoDePedidos.Api.Data
+{
+    public class GestaoDePedidosDbContext 
+        : DbContext
+    {
+        public GestaoDePedidosDbContext(DbContextOptions<GestaoDePedidosDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- GestaoDePedidos.Api/Program.cs
+++ GestaoDePedidos.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using GestaoDePedidos.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<GestaoDePedidosDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
