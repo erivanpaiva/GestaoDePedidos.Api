40749f6d038ac976070dd3f78c0165430d48b928
Erivan Paiva <jetpfilho@gmail.com>
2026-02-11 23:02:58 -0300

    feat: add initial customers and products data

--- GestaoDePedidos.Api/Program.cs
+++ GestaoDePedidos.Api/Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.EntityFrameworkCore;
 using GestaoDePedidos.Api.Data;
+using GestaoDePedidos.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.Design;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,51 @@ builder.Services.AddDbContext<GestaoDePedidosDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<GestaoDePedidosDbContext>();
+
+    if (!context.Customers.Any())
+    {
+        context.Customers.Add(
+            new Customer
+            {
+                Name = "Cliente 1",
+                Email = "cliente1@cliente.com",
+                CreatedAt = DateTime.UtcNow
+            }
+            );
+        context.SaveChanges();
+    }
+
+        if (!context.Products.Any())
+    {         context.Products.AddRange(
+            new Product
+            {
+                Name = "Teclado Mecânico Logitech MX",
+                Category = "Eletrônicos",
+                PriceCents = 100000,
+                Active = true
+            },
+            new Product
+            {
+                Name = "Smartphone iPhone 16",
+                Category = "Eletrônicos",
+                PriceCents = 450000,
+                Active = true
+            },
+            new Product
+            {
+                Name = "Mouse Gamer Razer DeathAdder Essential",
+                Category = "Eletrônicos",
+                PriceCents = 17900,
+                Active = true
+            }
+        );
+        context.SaveChanges();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
