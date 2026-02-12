8d5369bc133a4bb4869fb2a6606dce0c5925701b
Erivan Paiva <jetpfilho@gmail.com>
2026-02-11 21:20:20 -0300

    feat: add GET orders summary endpoint

---
+++ GestaoDePedidos.Api/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GestaoDePedidos.Api.Data;
+
+namespace GestaoDePedidos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController
+        : ControllerBase
+    {
+        private readonly GestaoDePedidosDbContext _context;
+        public OrdersController(GestaoDePedidosDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetOrders()
+        {
+            var orders = await _context.Orders.Select(o => new
+            {
+                o.Id,
+                o.Status,
+                o.CreatedAt,
+                Total = o.OrderItems.Sum(i => i.Quantity * i.UnitPriceCents)
+            }).ToListAsync();
+
+            return Ok(orders);
+        }
+    }
+}
